71cc7e5c95f2925403c37c793156cebc9fefb082
Joseph <napolitanjoe@gmail.com>
2022-04-01 12:21:17 -0400

    first commit, added template state for preview render

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import './App.css';
+import Main from './components/Main'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <h1>Get a job.</h1>
+          <h3>A CV-Application</h3>
+        </header>
+        <Main />
+        <footer className="footer">
+          <h2>napolitanj 2022</h2>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //editMode false is preview mode, editMode true is editMode
+      editMode: false,
+      generalInfo: {
+        id: 1,
+        firstName: 'Joe',
+        lastName: 'Dirt',
+        address: '1234 Hillbilly Ln.',
+        phone: '235-646-2423',
+        email: 'joedirt@gmail.com',
+        websites: {
+          web1: "github.com/dirtyjoe",
+          web2: "www.shootfireworksatit.org"
+        }
+      },
+      experience: {
+        id: 2,
+        title: "The Odin Project",
+        location: "Remote",
+        start: "Sept 2021",
+        end: "Present",
+        topic1: {
+          topic: "React",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in aliquet metus, non fermentum arcu. Integer tempus nibh eu urna tincidunt lacinia.",
+          id: Math.floor(Math.random() * 10000)+1
+        },
+        topic2: {
+          topic: "Javascript",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in aliquet metus, non fermentum arcu. Integer tempus nibh eu urna tincidunt lacinia.",
+          id: Math.floor(Math.random() * 10000)+1
+        },
+        topic3: {
+          topic: "HTML/CSS",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in aliquet metus, non fermentum arcu. Integer tempus nibh eu urna tincidunt lacinia.",
+          id: Math.floor(Math.random() * 10000)+1
+        }
+      },
+      employmentInfo:{
+        id: 3,
+        job1: {
+          employer: "Gas Station",
+          location: "CO",
+          position: "Attendant",
+          start: "Nov 2018",
+          end: "April 2021",
+          responsibilities: [
+            "Fill up gas for the people",
+            "Sell candy bars and sodas / give people diabetes",
+            "Devour tornados all day"
+          ],
+          id: Math.floor(Math.random() * 10000)+1
+        },
+        job2: {
+          employer: "Shoe-topia",
+          location: "Santa Fe, NM",
+          position: "Salesman",
+          start: "Nov 2015",
+          end: "April 2020",
+          responsibilities: [
+            "Learn about which shoes are the best",
+            "Put them on people's feet",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in aliquet metus, non fermentum arcu. Integer tempus nibh eu urna tincidunt lacinia."
+          ],
+          id: Math.floor(Math.random() * 10000)+1
+        },
+      },
+      education: {
+        id: 4,
+        education1: {
+          degree: "Full Stack Web Development",
+          location: "Remote",
+          start: "Sept 2021",
+          end: "Ongoing",
+          id: Math.floor(Math.random() * 10000)+1
+        },
+        education2: {
+          degree: "Full Stack Web Development",
+          location: "Remote",
+          start: "Sept 2021",
+          end: "Ongoing",
+          id: Math.floor(Math.random() * 10000)+1
+        },
+        education3: {
+          degree: "Full Stack Web Development",
+          location: "Remote",
+          start: "Sept 2021",
+          end: "Ongoing",
+          id: Math.floor(Math.random() * 10000)+1
+        },
+      },
+      skills: [
+        {
+          category: "Languages",
+          skillList: ["Javascript ES6", "HTML", "CSS"]
+        },
+        {
+          category: "Tools",
+          skillList: ["Javascript ES6", "HTML", "CSS","Javascript ES6", "HTML", "CSS","Javascript ES6", "HTML", "CSS"]
+        },  
+      ]
+    }
+  }
+  render() {
+    return (
+      <div class="mainBody">Main</div>
+    )
+  }
+}
+
+export default Main
