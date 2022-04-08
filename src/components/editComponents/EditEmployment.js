091d7db4687b9c767e0d4f551ad458b61e1f02f6
Joseph <napolitanjoe@gmail.com>
2022-04-08 10:32:11 -0400

    fix bug for modifying text, change to value

--- src/components/editComponents/EditEmployment.js
+++ src/components/editComponents/EditEmployment.js
@@ -9,24 +9,24 @@ class EditEmployment extends Component {
             <h2>Employment</h2>
             <hr style={{width:"100%", marginTop:"15px"}}></hr>
             {mainProps.employmentInfo.map(job =>
-                <div className="editExpCategory">
-                    <p>Position</p> 
-                    <input key={job.position} type="text" maxLength="60" defaultValue={job.position} onChange={(e) => editEmployment(e,job.id, 'position')}></input>
+                <div key={job}className="editExpCategory">
+                    <p>Position</p>
+                    <input maxLength="60" value={job.position} onChange={(e) => editEmployment(e,job.id, 'position')}></input>
                     <p>Employer</p>
-                    <input key={job.employer} type="text" maxLength="35" defaultValue={job.employer} onChange={(e) => editEmployment(e,job.id,'employer')}></input>
+                    <input maxLength="35" value={job.employer} onChange={(e) => editEmployment(e,job.id,'employer')}></input>
                     <p>Location</p>
-                    <input key={job.location} type="text" maxLength="21" defaultValue={job.location} onChange={(e) => editEmployment(e,job.id, 'location')}></input>
+                    <input maxLength="21" value={job.location} onChange={(e) => editEmployment(e,job.id, 'location')}></input>
                     <p>Start (month and year)</p>
-                    <input key={job.start} type="text" maxLength="14" defaultValue={job.start} onChange={(e) => editEmployment(e,job.id, 'start')}></input>
+                    <input maxLength="14" value={job.start} onChange={(e) => editEmployment(e,job.id, 'start')}></input>
                     <p>End (month and year)</p>
-                    <input key={job.end} type="text" maxLength="14" defaultValue={job.end} onChange={(e) => editEmployment(e,job.id, 'end')}></input>
+                    <input maxLength="14" value={job.end} onChange={(e) => editEmployment(e,job.id, 'end')}></input>
                     <div style={{display:"flex", alignItems:"center", gap:"5px"}}>
                         <p >Duties</p>
                         <p style={{color:'gray',cursor:'pointer'}} onClick={() => addNewDuty(mainProps.employmentInfo.indexOf(job))}>+ Add New</p>
                     </div>
                     {job.duties.map(duty =>
-                    <div key={duty} style={{display:"flex", alignItems:"center", gap:"5px"}}>
-                        <textarea type="text" maxLength="170" className="expInput" defaultValue={duty} onChange={(e) => editDuty(e,mainProps.employmentInfo.indexOf(job),job.duties.indexOf(duty))}></textarea>
+                    <div style={{display:"flex", alignItems:"center", gap:"5px"}}>
+                        <textarea maxLength="170" className="expInput" value={duty} onChange={(e) => editDuty(e,mainProps.employmentInfo.indexOf(job),job.duties.indexOf(duty))}></textarea>
                         <FaTimes 
                             style={{color:'gray',cursor:'pointer'}} onClick={() => deleteDuty(mainProps.employmentInfo.indexOf(job),job.duties.indexOf(duty))} 
                         />
